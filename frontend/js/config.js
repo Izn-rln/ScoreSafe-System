ad3d5de1f7ad3de062107254767cf4d66d52da14
Rollin Furio <rollin.furio@sorsu.edu.ph>
2026-05-12 22:16:04 +0800

    Resolved merge conflict and moved vercel.json to root

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-const API_BASE_URL = 'https://score-safe-system-52ow-git-main-izn-rlns-projects.vercel.app';
-=======
-const API_BASE_URL = 'https://score-safe-system-52ow-git-main-izn-rlns-projects.vercel.app';
->>>>>>> 82783284d4ae63485bb7245c403c77d187026c43
+const API_BASE_URL = "https://score-safe-system-52ow.vercel.app";
